431efa8302047522d7831b6719b54bda50371bb1
HOST <sh-kin@mail.ru>
2021-07-07 11:09:55 +0300

    squash 1 commit

--- app/src/main/java/com/shishkin/squash/MainActivity.kt
+++ app/src/main/java/com/shishkin/squash/MainActivity.kt
@@ -9,5 +9,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 //        first commit dev
 //        second commit dev
+//        squash 1 commit
     }
 }
